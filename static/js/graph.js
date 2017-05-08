556143dddf1e8bc494caee28c18353e2c112fc0a
Chris <christoal@outlook.com>
2017-05-08 11:43:00 +0800

    Connect to MongoDB

--- static/js/graph.js
+++ static/js/graph.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
         var typeGroup = typeDim.group();
 
 
-       //Line Chart for smaller screen
+        //Line Chart for smaller screen
         var lineChartSmall = dc.lineChart("#small-line-chart");
         lineChartSmall
             .width(340)
@@ -70,7 +70,7 @@ $(document).ready(function() {
             .yAxisLabel("Members");
 
 
-         //Line Chart for medium screen
+        //Line Chart for medium screen
         var lineChartMid = dc.lineChart("#mid-line-chart");
         lineChartMid
             .width(700)
@@ -111,7 +111,6 @@ $(document).ready(function() {
             });
 
 
-        //var colourScale = d3.scale.ordinal().range(["#0a9740", "#059d39", "#136629", "#113818", "#132412"])
         //Pie chart
         var pieChart = dc.pieChart("#pie-chart");
         pieChart
@@ -121,7 +120,6 @@ $(document).ready(function() {
             .innerRadius([50])
             .dimension(typeDim)
             .group(typeGroup)
-            //.colors(colourScale)
             .legend(dc.legend().x(240).y(0).gap(5));
 
 
@@ -287,106 +285,6 @@ $(document).ready(function() {
             .text(displayText);
         }
         AddXAxis(lineChartBig, "Rating");
-
-
-        //Assign maximum members to a variable
-        /*  var maxMembers = d3.max(animeData, function(d){return d.members;});
-
-         //Create a height scale
-         var heightScale = d3.scale.linear()
-         .domain([0, maxMembers])
-         .range([0,svgHeight]);
-
-         //Create a scale for yAxis
-         var yAxisScale = d3.scale.linear()
-         .domain([0, maxMembers])
-         .range([svgHeight, 0]);
-
-         //create a scale for xAxis
-         var xAxisScale = d3.scale.ordinal()
-         .domain(animeData.map(function(d) {return d.rating;}))
-         .rangeBands([0, svgWidth]);
-
-         //Create a color scale scale
-         var colorScale = d3.scale.linear()
-         .domain([0, maxMembers])
-         .range(["pink", "red"]);
-
-         //Create a tooltip
-         var tooltip = d3.select('#series-chart')
-         .append("div")
-         .classed("hidden", true)
-         .attr("id", "tooltip");
-
-         //Position yAxis and apply ticks
-         var yAxis = d3.svg.axis()
-         .scale(yAxisScale)
-         .orient("left")
-         .ticks(8);
-
-
-         //Position xAxis and apply ticks
-         var xAxis = d3.svg.axis()
-         .scale(xAxisScale)
-         .orient("bottom")
-         .ticks(animeData.length);
-
-
-         //Create canvas, append svg
-         var canvas = d3.select("#series-chart")
-         .append("svg")
-         .attr("id", "svgTooltip")
-         .attr("width",canvasWidth)
-         .attr("height",canvasHeight)
-         .attr("style","background-color:#2a2a2a");
-
-
-         canvas.append('g')
-         .attr("class", "axis")
-         .attr("transform", "translate(" + (margin.left) +","+ margin.bottom + ")")
-         .call(yAxis)
-         .append("text")
-         .attr("y", -10)
-         .style("text-anchor", "end")
-         .text("Members");
-
-         canvas.append("g")
-         .attr("class", "axis")
-         .attr("transform","translate("+ margin.left +","+ (canvasHeight - (margin.bottom)) + ")")
-         .call(xAxis)
-         .append("text")
-         .attr("y", 35)
-         .attr("x", 100)
-         .style("text-anchor", "middle")
-         .text("Rating");
-
-         var svg = canvas.append("g")
-         .attr("width",svgWidth)
-         .attr("height",svgHeight)
-         .attr("style","background-color:#2a2a2a")
-         .attr("transform","translate("+ margin.left +"," + margin.bottom + ")");
-
-         svg.selectAll('rect')
-         .data(animeData)
-         .enter()
-         .append("rect")
-         .attr("x", function(d, i) {return i * (svgWidth / animeData.length);})
-         .attr("y", function(d) {return svgHeight - heightScale(d.members);})
-         .attr('width', (svgWidth / animeData.length) - spacing)
-         .attr('height', function(d){return heightScale(d.members)})
-         .on("mouseover", function(d){
-         d3.select("#tooltip")
-         .style("left", (d3.event.pageX) + "px")
-         .style("top", (d3.event.pageY) + "px")
-         .classed("hidden", false);
-         tooltip.html(d.members + " members");
-         })
-         .on("mouseout", function(){
-         d3.select("#tooltip")
-         .classed("hidden", true);
-         })
-         .attr('fill', function(d){return(colorScale(d.members));});  */
-
 
     };
 });
