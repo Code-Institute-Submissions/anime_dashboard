b18879f2b8c07b87bebfad733b1deb69b3fc82e9
Chris <christoal@outlook.com>
2017-06-18 18:31:44 +0800

    Removed terminology table and replaced with tooltips on genre list

---
+++ static/js/ref.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Chris on 18/06/2017.
+ */
+//http://www.kriesi.at/archives/create-simple-tooltips-with-css-and-jquery
+
+function ref_tooltip(target, name) {
+    $(target).each(function (i) {
+        $("body").append("<div class='" + name + "' id='" + name + i + "'><p>" + $(this).attr('title') + "</p></div>");
+
+        var my_tooltip = $("#" + name + i);
+
+        if($(this).attr("title") != "" && $(this).attr("title") != "undefined" ){ // checks if there is a title
+
+            $(this).removeAttr("title").mouseover(function () {
+                my_tooltip.css({opacity: 0.8, display: "none"}).fadeIn(200);
+
+            }).mousemove(function (kmouse) {
+                var border_top = $(window).scrollTop();
+                var border_right = $(window).width();
+                var left_pos;
+                var top_pos;
+                var offset = 15;
+
+                if(border_right - (offset *2) >= my_tooltip.width() + kmouse.pageX){
+					left_pos = kmouse.pageX+offset;
+					} else{
+					left_pos = border_right-my_tooltip.width()-offset;
+					}
+
+				if(border_top + (offset *2)>= kmouse.pageY - my_tooltip.height()){
+					top_pos = border_top +offset;
+                }else{
+					top_pos = kmouse.pageY-my_tooltip.height()-offset;
+                }
+
+                my_tooltip.css({left:left_pos, top:top_pos});
+
+            }).mouseout(function () {
+                my_tooltip.fadeOut(200);
+            });
+
+        }
+
+
+    });
+}
+
+$(document).ready(function(){
+	 ref_tooltip("td","tooltip");
+});
